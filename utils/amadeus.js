11c40ec9d0acb4efb942574bf65075cb2218ea4b
kollawa <126017447+kollawa@users.noreply.github.com>
2023-04-29 02:31:02 +0300

    Update amadeus.js

--- utils/amadeus.js
+++ utils/amadeus.js
@@ -3,7 +3,6 @@ const Amadeus = require('amadeus');
 const amadeusApi = new Amadeus({
   clientId: 'scaTkOPIdKkhQb8bY0w8tyIvqxV21JFM',
   clientSecret: 'oVAkrqbm7Cm2LyEr',
-  baseUrl: 'https://api.amadeus.com'
 });
 
 module.exports = amadeusApi;
