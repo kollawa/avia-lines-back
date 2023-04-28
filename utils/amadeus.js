591d09d513c4b4c93f6110d66db491975ccad685
MichaelCYSA <82159905+MichaelCYSA@users.noreply.github.com>
2023-04-29 01:23:45 +0300

    Update amadeus.js

--- utils/amadeus.js
+++ utils/amadeus.js
@@ -2,7 +2,8 @@ const Amadeus = require('amadeus');
 
 const amadeusApi = new Amadeus({
   clientId: 'scaTkOPIdKkhQb8bY0w8tyIvqxV21JFM',
-  clientSecret: 'oVAkrqbm7Cm2LyEr'
+  clientSecret: 'oVAkrqbm7Cm2LyEr',
+  baseUrl: 'https://api.amadeus.com'
 });
 
-module.exports = amadeusApi
+module.exports = amadeusApi;
